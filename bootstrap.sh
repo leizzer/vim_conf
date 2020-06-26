12d55aa0a10867afc6c7443cbb918439fc8407bd
leizzer <cecristianre@gmail.com>
2020-06-26 12:12:30 -0300

    wip

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,20 +1,5 @@
 #!/bin/sh
 
-# Install vim
-sudo apt-get install -y vim
-
-# Install ctag for project managment
-sudo apt-get install -y exuberant-ctags
-
-# Bundle install
-vim -c BundleInstall! -c q -c q -u bundles.vim
-
-# Create ~/.vimrc link to ~/.vim/vimrc
-if [ -f ~/.vimrc ]
-then
-  mv ~/.vimrc ~/.vimrc_old
+if ! pip3 show pynvim
+  pip3 install --user pynvim
 fi
-ln -s ~/.vim/vimrc ~/.vimrc 
-
-# Create tmp directory for .swp files
-mkdir ~/.vim/tmp
